c739ed294bdd7fbb20b40b6c368a140e04a6c823
Mark Mesander <mark_mesander@hotmail.com>
2023-12-17 19:18:45 +0100

    first commit

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/RevitalizedWebshopApplication.java
@@ -0,0 +1,13 @@
+package nu.revitalized.revitalizedwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevitalizedWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevitalizedWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nu/revitalized/revitalizedwebshop/RevitalizedWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nu.revitalized.revitalizedwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevitalizedWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
