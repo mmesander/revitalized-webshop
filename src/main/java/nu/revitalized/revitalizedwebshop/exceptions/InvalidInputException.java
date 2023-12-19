1fefa3be75c4dff6e280e382d8e621e590a99537
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 17:10:54 +0100

    add methods

--- src/main/java/nu/revitalized/revitalizedwebshop/exceptions/InvalidInputException.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/exceptions/InvalidInputException.java
@@ -1,4 +1,11 @@
 package nu.revitalized.revitalizedwebshop.exceptions;
 
-public class InvalidInputException {
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
 }
