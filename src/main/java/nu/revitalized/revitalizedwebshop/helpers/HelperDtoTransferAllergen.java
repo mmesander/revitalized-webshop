72cc0bbaa6f521d091ac454a2b39ff102149f634
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 18:04:26 +0100

    add methods

--- src/main/java/nu/revitalized/revitalizedwebshop/helpers/HelperDtoTransferAllergen.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/helpers/HelperDtoTransferAllergen.java
@@ -1,4 +1,24 @@
 package nu.revitalized.revitalizedwebshop.helpers;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.dtos.input.AllergenInputDto;
+import nu.revitalized.revitalizedwebshop.dtos.output.AllergenDto;
+import nu.revitalized.revitalizedwebshop.models.Allergen;
+
 public class HelperDtoTransferAllergen {
+    public static Allergen transferToAllergen(AllergenInputDto inputDto) {
+        Allergen allergen = new Allergen();
+
+        allergen.setName(inputDto.getName());
+
+        return allergen;
+    }
+
+    public static AllergenDto transferAllergenToDto(Allergen allergen) {
+        AllergenDto allergenDto = new AllergenDto();
+
+        allergenDto.setName(allergen.getName());
+
+        return allergenDto;
+    }
 }
