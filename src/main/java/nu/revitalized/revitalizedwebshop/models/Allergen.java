718ec91a698773ade87afac3645ed5b4e721135a
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 17:31:02 +0100

    add variables

--- src/main/java/nu/revitalized/revitalizedwebshop/models/Allergen.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/models/Allergen.java
@@ -1,4 +1,22 @@
 package nu.revitalized.revitalizedwebshop.models;
 
+// Imports
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "allergens")
 public class Allergen {
+    // Variables
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    // Relations
+//    private Set<Supplement> supplements;
 }
