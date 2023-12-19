9e99cd2a6476ac41677ce165f1d51037af15731e
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 17:13:10 +0100

    add invalid input exception method

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/ExceptionController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/ExceptionController.java
@@ -1,5 +1,6 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
+import nu.revitalized.revitalizedwebshop.exceptions.InvalidInputException;
 import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,5 +12,10 @@ public class ExceptionController {
     @ExceptionHandler(value = RecordNotFoundException.class)
     public ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = InvalidInputException.class)
+    public ResponseEntity<Object> handleInvalidInputException(InvalidInputException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
