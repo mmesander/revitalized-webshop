3581a164c41dcdf19ba8b3ea1f1bc8ad6c370a61
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 16:33:12 +0100

    add Supplement sub class

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/models/Supplement.java
@@ -0,0 +1,12 @@
+package nu.revitalized.revitalizedwebshop.models;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Supplement extends Product {
+    // Variables
+    private String contains;
+}
