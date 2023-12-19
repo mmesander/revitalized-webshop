4bc7a885ca3e41c92c828bfb807818050aaa8c9d
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 17:39:52 +0100

    inject repository

--- src/main/java/nu/revitalized/revitalizedwebshop/services/AllergenService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/AllergenService.java
@@ -1,4 +1,18 @@
 package nu.revitalized.revitalizedwebshop.services;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.repositories.AllergenRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AllergenService {
+    private final AllergenRepository allergenRepository;
+
+    public AllergenService(AllergenRepository allergenRepository) {
+        this.allergenRepository = allergenRepository;
+    }
+
+
+    // Get Methods
+
 }
