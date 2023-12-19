8c8800f36766d970300d179fae49382be2052c09
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 23:43:47 +0100

    import service

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/AllergenController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/AllergenController.java
@@ -1,4 +1,16 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.services.AllergenService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AllergenController {
+    private final AllergenService allergenService;
+
+    public AllergenController(AllergenService allergenService) {
+        this.allergenService = allergenService;
+    }
+
+    // Get Requests
 }
