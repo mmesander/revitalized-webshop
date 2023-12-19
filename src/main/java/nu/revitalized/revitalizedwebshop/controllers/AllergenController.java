dffc02fcb004f8db6cff46543db741681c308805
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 23:46:42 +0100

    add controller for all allergens

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/AllergenController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/AllergenController.java
@@ -1,8 +1,13 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
+import nu.revitalized.revitalizedwebshop.dtos.output.AllergenDto;
 import nu.revitalized.revitalizedwebshop.services.AllergenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AllergenController {
@@ -12,5 +17,12 @@ public class AllergenController {
         this.allergenService = allergenService;
     }
 
+
     // Get Requests
+    @GetMapping("/supplementen/allergenen")
+    public ResponseEntity<List<AllergenDto>> getAllAllergens() {
+        List<AllergenDto> dtos = allergenService.getAllAllergens();
+
+        return ResponseEntity.ok().body(dtos);
+    }
 }
