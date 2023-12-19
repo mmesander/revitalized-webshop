4d6cb6271edd8a7e2691fc3b738053682f3b0336
Mark Mesander <mark_mesander@hotmail.com>
2023-12-19 17:00:09 +0100

    add SupplementRepository

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
@@ -0,0 +1,13 @@
+package nu.revitalized.revitalizedwebshop.repositories;
+
+import nu.revitalized.revitalizedwebshop.models.Supplement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SupplementRepository extends JpaRepository<Supplement, Long> {
+    List<Supplement> findSupplementByName(String name);
+    List<Supplement> findSupplementsByBrand(String brand);
+    List<Supplement> findSupplementsByBrandAndName(String brand, String name);
+    List<Supplement> findSupplementsByPriceBefore(Double price);
+}
