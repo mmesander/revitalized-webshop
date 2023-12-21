283e6e72b7dce852ba2b9a631de349aa04e0f612
mmesander <mark_mesander@hotmail.com>
2023-12-21 14:39:53 +0100

    add inputDto and outputDto

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/SupplementInputDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/SupplementInputDto.java
@@ -1,4 +1,16 @@
 package nu.revitalized.revitalizedwebshop.dtos.input;
 
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SupplementInputDto {
+    private String name;
+    private String brand;
+    private String description;
+    private Double price;
+    private Double averageRating;
+    private String contains;
 }
--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/SupplementDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/SupplementDto.java
@@ -1,4 +1,17 @@
 package nu.revitalized.revitalizedwebshop.dtos.output;
 
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SupplementDto {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private Double price;
+    private Double averageRating;
+    private String contains;
 }
