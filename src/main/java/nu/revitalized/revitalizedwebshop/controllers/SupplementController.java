b50b0eca93913c270f8d6fd7c88d27ca9af6b553
Mark Mesander <mark_mesander@hotmail.com>
2023-12-21 21:23:35 +0100

    add CRUD Requests

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/SupplementController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/SupplementController.java
@@ -1,12 +1,21 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
+
+import jakarta.validation.Valid;
+import nu.revitalized.revitalizedwebshop.dtos.input.SupplementInputDto;
 import nu.revitalized.revitalizedwebshop.dtos.output.SupplementDto;
+import nu.revitalized.revitalizedwebshop.exceptions.InvalidInputException;
 import nu.revitalized.revitalizedwebshop.services.SupplementService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static nu.revitalized.revitalizedwebshop.helpers.BindingResultHelper.handleBindingResultError;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 public class SupplementController {
@@ -17,7 +26,7 @@ public class SupplementController {
     }
 
 
-    // Crud Requests
+    // CRUD Requests
     @GetMapping("/supplementen")
     public ResponseEntity<List<SupplementDto>> getAllSupplements() {
         List<SupplementDto> dtos = supplementService.getAllSupplements();
@@ -54,5 +63,60 @@ public class SupplementController {
         return ResponseEntity.ok().body(dtos);
     }
 
+    @PostMapping("/supplementen")
+    public ResponseEntity<SupplementDto> createSupplement(
+            @Valid
+            @RequestBody SupplementInputDto inputDto,
+            BindingResult bindingResult
+    ) {
+        SupplementDto dto;
 
+        if (bindingResult.hasFieldErrors()) {
+            throw new InvalidInputException(handleBindingResultError(bindingResult));
+        } else {
+            dto = supplementService.createSupplement(inputDto);
+            URI uri = URI.create(
+                    ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/" + dto.getId()).toUriString());
+            return ResponseEntity.created(uri).body(dto);
+        }
+    }
+
+    @PutMapping("/supplementen/{id}")
+    public ResponseEntity<SupplementDto> updateSupplement(
+            @PathVariable("id") Long id,
+            @Valid
+            @RequestBody SupplementInputDto inputDto,
+            BindingResult bindingResult
+    ) {
+        SupplementDto dto;
+
+        if (bindingResult.hasFieldErrors()) {
+            throw new InvalidInputException(handleBindingResultError(bindingResult));
+        } else {
+            dto = supplementService.updateSupplement(id, inputDto);
+        }
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PatchMapping("/supplementen/{id}")
+    public ResponseEntity<SupplementDto> patchSupplement(
+            @PathVariable("id") Long id,
+            @RequestBody SupplementInputDto inputDto
+    ) {
+        SupplementDto dto = supplementService.patchSupplement(id, inputDto);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/supplementen/{id}")
+    public ResponseEntity<Object> deleteSupplement(
+            @PathVariable("id") Long id
+    ) {
+        supplementService.deleteSupplement(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
