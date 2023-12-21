3afd73071035e6b4d287c7b93ce8d1d18a09dc62
Mark Mesander <mark_mesander@hotmail.com>
2023-12-21 20:48:31 +0100

    add service

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/SupplementController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/SupplementController.java
@@ -1,4 +1,19 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.services.SupplementService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SupplementController {
+    private final SupplementService supplementService;
+
+    public SupplementController(SupplementService supplementService) {
+        this.supplementService = supplementService;
+    }
+
+
+    // Crud Requests
+
+
 }
