af748c67bb3966e9f4ef24233e3f647a84a67662
mmesander <mark_mesander@hotmail.com>
2023-12-21 15:37:37 +0100

    add get requests

--- src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
@@ -2,9 +2,14 @@ package nu.revitalized.revitalizedwebshop.services;
 
 import nu.revitalized.revitalizedwebshop.dtos.input.SupplementInputDto;
 import nu.revitalized.revitalizedwebshop.dtos.output.SupplementDto;
+import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
 import nu.revitalized.revitalizedwebshop.models.Supplement;
 import nu.revitalized.revitalizedwebshop.repositories.SupplementRepository;
 import static nu.revitalized.revitalizedwebshop.helpers.HelperCopyProperties.copyProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 public class SupplementService {
@@ -30,5 +35,33 @@ public class SupplementService {
         copyProperties(supplement, supplementDto);
 
         return supplementDto;
+    }
+
+
+    // Get Methods
+    public List<SupplementDto> getAllSupplements() {
+        List<Supplement> supplements = supplementRepository.findAll();
+        List<SupplementDto> supplementDtos = new ArrayList<>();
+
+        for (Supplement supplement : supplements) {
+            SupplementDto supplementDto = transferToSupplementDto(supplement);
+            supplementDtos.add(supplementDto);
+        }
+
+        if (supplementDtos.isEmpty()) {
+            throw new RecordNotFoundException("No supplements are found");
+        } else {
+            return supplementDtos;
+        }
+    }
+
+    public SupplementDto getSupplementById(Long id) {
+        Optional<Supplement> supplement = supplementRepository.findById(id);
+
+        if (supplement.isPresent()) {
+            return transferToSupplementDto(supplement.get());
+        } else {
+            throw new RecordNotFoundException("No supplement found with id: " + id);
+        }
     }
 }
