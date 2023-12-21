65b1ad0214ba8795f8825034181642e36147c1b2
mmesander <mark_mesander@hotmail.com>
2023-12-21 15:12:26 +0100

    import repository

--- src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
@@ -1,5 +1,13 @@
 package nu.revitalized.revitalizedwebshop.services;
 
+import nu.revitalized.revitalizedwebshop.repositories.SupplementRepository;
+
 public class SupplementService {
+    private final SupplementRepository supplementRepository;
+
+    public SupplementService(SupplementRepository supplementRepository) {
+        this.supplementRepository = supplementRepository;
+    }
+
 
 }
