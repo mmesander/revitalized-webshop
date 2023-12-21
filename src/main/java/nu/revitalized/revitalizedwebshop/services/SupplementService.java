8b26a7c4b2901743cc5612c516e30c82d1bf9569
mmesander <mark_mesander@hotmail.com>
2023-12-21 15:15:16 +0100

    add transfer methods

--- src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/SupplementService.java
@@ -1,6 +1,11 @@
 package nu.revitalized.revitalizedwebshop.services;
 
+import nu.revitalized.revitalizedwebshop.dtos.input.SupplementInputDto;
+import nu.revitalized.revitalizedwebshop.dtos.output.SupplementDto;
+import nu.revitalized.revitalizedwebshop.models.Supplement;
 import nu.revitalized.revitalizedwebshop.repositories.SupplementRepository;
+import static nu.revitalized.revitalizedwebshop.helpers.HelperCopyProperties.copyProperties;
+
 
 public class SupplementService {
     private final SupplementRepository supplementRepository;
@@ -10,4 +15,20 @@ public class SupplementService {
     }
 
 
+    // Transfer Methods
+    public Supplement transferToSupplement(SupplementInputDto inputDto) {
+        Supplement supplement = new Supplement();
+
+        copyProperties(inputDto, supplement);
+
+        return supplement;
+    }
+
+    public SupplementDto transferToSupplementDto(Supplement supplement) {
+        SupplementDto supplementDto = new SupplementDto();
+
+        copyProperties(supplement, supplementDto);
+
+        return supplementDto;
+    }
 }
