7e17f12341317b8872212e7988f88d489cb01683
Mark Mesander <mark_mesander@hotmail.com>
2024-01-19 12:21:25 +0100

    add relations in dtos

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/AllergenDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/AllergenDto.java
@@ -4,9 +4,16 @@ package nu.revitalized.revitalizedwebshop.dtos.output;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 public class AllergenDto {
+    // Variables
     private Long id;
     private String name;
+
+
+    // Relations
+    private Set<SupplementDto> supplements;
 }
--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/SupplementDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/SupplementDto.java
@@ -4,9 +4,12 @@ package nu.revitalized.revitalizedwebshop.dtos.output;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 public class SupplementDto {
+    // Variables
     private Long id;
     private String name;
     private String brand;
@@ -14,4 +17,8 @@ public class SupplementDto {
     private Double price;
     private Double averageRating;
     private String contains;
+
+
+    // Relations
+    private Set<AllergenDto> allergens;
 }
