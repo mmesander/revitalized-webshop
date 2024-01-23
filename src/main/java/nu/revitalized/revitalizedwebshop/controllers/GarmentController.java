678302c4ae274ba6411b68677c24c161c98c1005
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 17:17:00 +0100

    inject service in controller

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
@@ -1,4 +1,21 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.services.GarmentService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GarmentController {
+    private final GarmentService garmentService;
+
+    public GarmentController(GarmentService garmentService) {
+        this.garmentService = garmentService;
+    }
+
+
+    // CRUD Requests -- GET Requests
+    // CRUD Requests -- POST Requests
+    // CRUD Requests -- PUT/PATCH Requests
+    // CRUD Requests -- DELETE Requests
+    // Relations Requests
 }
