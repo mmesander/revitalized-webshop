6930790f2b3c57669da7c3af9e6371cba930a6c3
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 19:02:14 +0100

    inject productService

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/ProductController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/ProductController.java
@@ -1,4 +1,18 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
+// Imports
+import nu.revitalized.revitalizedwebshop.services.ProductService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    // CRUD Requests -- GET Requests
+    // CRUD Requests -- DELETE Requests
 }
