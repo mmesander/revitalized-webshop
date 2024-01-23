fb6c2382b498d024a74781cf19d98059f451fb4a
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 17:15:55 +0100

    update note

--- src/main/java/nu/revitalized/revitalizedwebshop/VOLGORDE.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/VOLGORDE.java
@@ -8,22 +8,20 @@ public class VOLGORDE {
     // Model
     // Repository
 
-    // Get all
-    // Get by id
-    // Get specifiek
-    // Post
-    // Put
-    // Patch
-    // Delete
-
-    //TODO: in product moet nog een img komen en reviews
-
     // Transfer Methods
     // CRUD Methods --> GET Methods
     // CRUD Methods --> POST Methods
     // CRUD Methods --> PUT/PATCH Methods
     // CRUD Methods --> DELETE Methods
     // Relations Methods
+
+    // CRUD Requests -- GET Requests
+    // CRUD Requests -- POST Requests
+    // CRUD Requests -- PUT/PATCH Requests
+    // CRUD Requests -- DELETE Requests
+    // Relations Requests
+
+    //TODO: in product moet nog een img komen en reviews
 
     //TODO: ROWAN
     //TODO: hoe maak ik hem ignore case (in supplement en garment)
