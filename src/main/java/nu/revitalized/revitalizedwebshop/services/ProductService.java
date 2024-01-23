494b49a597c8362ddad34185d19683d0d5a734d0
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 18:40:39 +0100

    add transfer methods

--- src/main/java/nu/revitalized/revitalizedwebshop/services/ProductService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/ProductService.java
@@ -1,6 +1,9 @@
 package nu.revitalized.revitalizedwebshop.services;
 
 // Imports
+import static nu.revitalized.revitalizedwebshop.helpers.CopyPropertiesHelper.copyProperties;
+import nu.revitalized.revitalizedwebshop.dtos.output.ProductDto;
+import nu.revitalized.revitalizedwebshop.models.Garment;
 import nu.revitalized.revitalizedwebshop.models.Supplement;
 import nu.revitalized.revitalizedwebshop.repositories.GarmentRepository;
 import nu.revitalized.revitalizedwebshop.repositories.SupplementRepository;
@@ -21,8 +24,34 @@ public class ProductService {
         this.garmentRepository = garmentRepository;
     }
 
+
+    // Transfer Methods
+    public static ProductDto supplementToProductDto(Supplement supplement) {
+        ProductDto productDto = new ProductDto();
+
+        copyProperties(supplement, productDto);
+
+        return productDto;
+    }
+
+    public static ProductDto garmentToProductDto(Garment garment) {
+        ProductDto productDto = new ProductDto();
+
+        copyProperties(garment, productDto);
+
+        return productDto;
+    }
+
+
     // CRUD Methods --> GET Methods
-    
+    public ProductDto getAllProducts() {
+        List<Supplement> supplements = supplementRepository.findAll();
+        List<Garment> garments = garmentRepository.findAll();
+
+        for (Supplement supplement : supplements) {
+
+        }
+    }
 
     // CRUD Methods --> POST Methods
     // CRUD Methods --> PUT/PATCH Methods
