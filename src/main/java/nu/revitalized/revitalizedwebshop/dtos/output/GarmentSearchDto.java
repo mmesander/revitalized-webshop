c919437d2cc993660cbab1a6272ed4252e17d0b6
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 16:47:48 +0100

    add variables to garmentsearchdto

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/GarmentSearchDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/GarmentSearchDto.java
@@ -1,4 +1,17 @@
 package nu.revitalized.revitalizedwebshop.dtos.output;
 
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GarmentSearchDto {
+    // Variables
+    private String name;
+    private String brand;
+    private Double price;
+    private Double averageRating;
+    private String size;
+    private String color;
 }
