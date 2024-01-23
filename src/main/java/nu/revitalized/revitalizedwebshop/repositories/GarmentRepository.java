f815be0747b787c28a9dc21601c6124bb5150e76
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 12:57:00 +0100

    fix problem with repo

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface GarmentRepository extends JpaRepository<Garment, Long> {
     List<Garment> findGarmentsByColorContainsIgnoreCase(String color);
     List<Garment> findGarmentsBySizeContainsIgnoreCase(String size);
-    List<Garment> findGarmentsByColorContainsIgnoreCaseAndSizeContainsIgnoreCase(String color, String size)
+    List<Garment> findGarmentsByColorContainsIgnoreCaseAndSizeContainsIgnoreCase(String color, String size);
     List<Garment> findGarmentByPriceBefore(Double price);
 }
