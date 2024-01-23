4f72308468b9224baeecec0d5e1862481311b62d
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 17:22:40 +0100

    add getGarmentsByParam controller

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
@@ -2,10 +2,12 @@ package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
 import nu.revitalized.revitalizedwebshop.dtos.output.GarmentDto;
+import nu.revitalized.revitalizedwebshop.dtos.output.SearchDto;
 import nu.revitalized.revitalizedwebshop.services.GarmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -36,7 +38,28 @@ public class GarmentController {
         return ResponseEntity.ok().body(dto);
     }
 
+    @GetMapping("/kleding/zoeken")
+    public ResponseEntity<List<GarmentDto>> getGarmentsByParam(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) Double price,
+            @RequestParam(required = false) Double averageRating,
+            @RequestParam(required = false) String size,
+            @RequestParam(required = false) String color
+    ) {
+        SearchDto searchDto = new SearchDto();
 
+        searchDto.setName(name);
+        searchDto.setBrand(brand);
+        searchDto.setPrice(price);
+        searchDto.setAverageRating(averageRating);
+        searchDto.setSize(size);
+        searchDto.setColor(color);
+
+        List<GarmentDto> dtos = garmentService.getGarmentsByParam(searchDto);
+
+        return ResponseEntity.ok().body(dtos);
+    }
 
     // CRUD Requests -- POST Requests
     // CRUD Requests -- PUT/PATCH Requests
