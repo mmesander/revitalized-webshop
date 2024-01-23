aa9c6bf4988d9efa55a708cfc75a4f07a92d802b
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 16:15:31 +0100

    remove old imports

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
@@ -5,8 +5,6 @@ import nu.revitalized.revitalizedwebshop.models.Supplement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface SupplementRepository extends JpaRepository<Supplement, Long> {
