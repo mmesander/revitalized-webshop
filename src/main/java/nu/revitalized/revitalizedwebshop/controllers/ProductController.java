c31cfdd44a43e8fee4e5c537d29055e5a5afe8bd
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 19:05:16 +0100

    add get requests

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/ProductController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/ProductController.java
@@ -1,8 +1,14 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
+import nu.revitalized.revitalizedwebshop.dtos.output.ProductDto;
 import nu.revitalized.revitalizedwebshop.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -14,5 +20,21 @@ public class ProductController {
 
 
     // CRUD Requests -- GET Requests
+    @GetMapping("/producten")
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        List<ProductDto> dtos = productService.getAllProducts();
+
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @GetMapping("/producten/{id}")
+    public ResponseEntity<Object> getProductById(
+            @PathVariable("id") Long id
+    ) {
+        Object dto = productService.getProductById(id);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
     // CRUD Requests -- DELETE Requests
 }
