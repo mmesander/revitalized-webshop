d0358f5b9777498dc019d57244239bb7b622e2c9
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 17:19:49 +0100

    add controller for getAllGarments and getGarmentById

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
@@ -1,8 +1,14 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
+import nu.revitalized.revitalizedwebshop.dtos.output.GarmentDto;
 import nu.revitalized.revitalizedwebshop.services.GarmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GarmentController {
@@ -14,6 +20,24 @@ public class GarmentController {
 
 
     // CRUD Requests -- GET Requests
+    @GetMapping("/kleding")
+    public ResponseEntity<List<GarmentDto>> getAllGarments() {
+        List<GarmentDto> dtos = garmentService.getAllGarments();
+
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @GetMapping("/kleding/{id}")
+    public ResponseEntity<GarmentDto> getGarmentById(
+            @PathVariable("id") Long id
+    ) {
+        GarmentDto dto = garmentService.getGarmentById(id);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+
+
     // CRUD Requests -- POST Requests
     // CRUD Requests -- PUT/PATCH Requests
     // CRUD Requests -- DELETE Requests
