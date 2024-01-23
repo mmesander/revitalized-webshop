204eb297b7a0ab21e9b1f6bbf159e9cda216fd94
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 13:00:00 +0100

    inject garment repo in service

--- src/main/java/nu/revitalized/revitalizedwebshop/services/GarmentService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/GarmentService.java
@@ -1,4 +1,14 @@
 package nu.revitalized.revitalizedwebshop.services;
 
+import nu.revitalized.revitalizedwebshop.models.Garment;
+import nu.revitalized.revitalizedwebshop.repositories.GarmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GarmentService {
+    private final GarmentRepository garmentRepository;
+
+    public GarmentService(GarmentRepository garmentRepository) {
+        this.garmentRepository = garmentRepository;
+    }
 }
