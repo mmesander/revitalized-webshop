7ecc1d16f45c202c3d8027c4801a85a37e6ceab5
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 18:43:52 +0100

    add getAllProducts method

--- src/main/java/nu/revitalized/revitalizedwebshop/services/ProductService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/ProductService.java
@@ -3,12 +3,14 @@ package nu.revitalized.revitalizedwebshop.services;
 // Imports
 import static nu.revitalized.revitalizedwebshop.helpers.CopyPropertiesHelper.copyProperties;
 import nu.revitalized.revitalizedwebshop.dtos.output.ProductDto;
+import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
 import nu.revitalized.revitalizedwebshop.models.Garment;
 import nu.revitalized.revitalizedwebshop.models.Supplement;
 import nu.revitalized.revitalizedwebshop.repositories.GarmentRepository;
 import nu.revitalized.revitalizedwebshop.repositories.SupplementRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -44,12 +46,25 @@ public class ProductService {
 
 
     // CRUD Methods --> GET Methods
-    public ProductDto getAllProducts() {
+    public List<ProductDto> getAllProducts() {
         List<Supplement> supplements = supplementRepository.findAll();
         List<Garment> garments = garmentRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
 
         for (Supplement supplement : supplements) {
+            ProductDto productDto = supplementToProductDto(supplement);
+            productDtos.add(productDto);
+        }
 
+        for (Garment garment : garments) {
+            ProductDto productDto = garmentToProductDto(garment);
+            productDtos.add(productDto);
+        }
+
+        if (productDtos.isEmpty()) {
+            throw new RecordNotFoundException("No products found");
+        } else {
+            return productDtos;
         }
     }
 
