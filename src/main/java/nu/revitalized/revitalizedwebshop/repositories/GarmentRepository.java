df6b1eeb3c5d939445923b423b18bbca59118b8e
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 12:56:42 +0100

    update garment repository

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GarmentRepository extends JpaRepository<Garment, Long> {
-    List<Garment> findGarmentsByColor(String color);
-    List<Garment> findGarmentsBySize(String size);
+    List<Garment> findGarmentsByColorContainsIgnoreCase(String color);
+    List<Garment> findGarmentsBySizeContainsIgnoreCase(String size);
+    List<Garment> findGarmentsByColorContainsIgnoreCaseAndSizeContainsIgnoreCase(String color, String size)
     List<Garment> findGarmentByPriceBefore(Double price);
 }
