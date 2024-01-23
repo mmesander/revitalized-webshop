8c268c4d680b4838e7a62cb6d17a5c34ea384c98
Mark Mesander <mark_mesander@hotmail.com>
2024-01-23 13:43:51 +0100

    add search by id

--- src/main/java/nu/revitalized/revitalizedwebshop/services/GarmentService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/GarmentService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GarmentService {
@@ -56,8 +57,21 @@ public class GarmentService {
         }
     }
 
-    // Transfer Methods
-    // CRUD Methods --> GET Methods
+    public GarmentDto getGarmentById(Long id) {
+        Optional<Garment> garment = garmentRepository.findById(id);
+
+        if (garment.isPresent()) {
+            return garmentToDto(garment.get());
+        } else {
+            throw new RecordNotFoundException("No garment found with id: " + id);
+        }
+    }
+
+//    public List<GarmentDto> getGarmentsByColorAndOrSize(String color, String size) {}
+//    public List<GarmentDto> getGarmentsByColor(String color) {}
+//    public List<GarmentDto> getGarmentsBySize(String size) {}
+//    public List<GarmentDto> getGarmentsByPriceBefore(Double price) {}
+
     // CRUD Methods --> POST Methods
     // CRUD Methods --> PUT/PATCH Methods
     // CRUD Methods --> DELETE Methods
