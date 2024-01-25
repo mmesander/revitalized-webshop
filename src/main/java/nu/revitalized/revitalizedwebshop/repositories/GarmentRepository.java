87e9465a94d7076726b651a9b8bf11f433c24bbf
Mark Mesander <mark_mesander@hotmail.com>
2024-01-25 19:51:24 +0100

    remove unused method

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/GarmentRepository.java
@@ -8,20 +8,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface GarmentRepository extends JpaRepository<Garment, Long> {
-
-    @Query("SELECT g FROM Garment g WHERE " +
-            "(:name is null or lower(g.name) like %:name%) and " +
-            "(:brand is null or lower(g.brand) like %:brand%) and " +
-            "(:price is null or g.price <= :price) and " +
-            "(:averageRating is null or g.averageRating >= :averageRating) and " +
-            "(:size is null or lower(g.size) like %:size%) and " +
-            "(:color is null or lower(g.color) like %:color%)")
-    List<Garment> findGarmentsByCriteria(
-            @Param("name") String name,
-            @Param("brand") String brand,
-            @Param("price") Double price,
-            @Param("averageRating") Double averageRating,
-            @Param("size") String size,
-            @Param("color") String color
-    );
 }
