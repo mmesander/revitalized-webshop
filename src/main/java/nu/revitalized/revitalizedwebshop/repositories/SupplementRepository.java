c61f4de7bcc465b7154957f28f65155f59c8d44b
Mark Mesander <mark_mesander@hotmail.com>
2024-01-25 17:50:33 +0100

    add specification to repo

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
@@ -3,11 +3,12 @@ package nu.revitalized.revitalizedwebshop.repositories;
 // Imports
 import nu.revitalized.revitalizedwebshop.models.Supplement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-public interface SupplementRepository extends JpaRepository<Supplement, Long> {
+public interface SupplementRepository extends JpaRepository<Supplement, Long>, JpaSpecificationExecutor<Supplement> {
     List<Supplement> findSupplementsByPriceLessThanEqual(Double price);
 
     @Query("SELECT s FROM Supplement s WHERE " +
