71fa01d6e3253b021c0619cf0113bfa111b19b02
Mark Mesander <mark_mesander@hotmail.com>
2024-01-25 20:24:17 +0100

    remove unused methods and imports

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/GarmentController.java
@@ -1,12 +1,9 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
-
 import static nu.revitalized.revitalizedwebshop.helpers.BindingResultHelper.handleBindingResultError;
-
 import nu.revitalized.revitalizedwebshop.dtos.input.GarmentInputDto;
 import nu.revitalized.revitalizedwebshop.dtos.output.GarmentDto;
-import nu.revitalized.revitalizedwebshop.dtos.output.SearchDto;
 import nu.revitalized.revitalizedwebshop.exceptions.InvalidInputException;
 import nu.revitalized.revitalizedwebshop.services.GarmentService;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 import java.util.List;
 
@@ -64,32 +60,6 @@ public class GarmentController {
 
         return ResponseEntity.ok().body(dtos);
     }
-
-
-
-
-//    @GetMapping("/producten/kleding/zoeken")
-//    public ResponseEntity<List<GarmentDto>> getGarmentsByParam(
-//            @RequestParam(required = false) String name,
-//            @RequestParam(required = false) String brand,
-//            @RequestParam(required = false) Double price,
-//            @RequestParam(required = false) Double averageRating,
-//            @RequestParam(required = false) String size,
-//            @RequestParam(required = false) String color
-//    ) {
-//        SearchDto searchDto = new SearchDto();
-//
-//        searchDto.setName(name);
-//        searchDto.setBrand(brand);
-//        searchDto.setPrice(price);
-//        searchDto.setAverageRating(averageRating);
-//        searchDto.setSize(size);
-//        searchDto.setColor(color);
-//
-//        List<GarmentDto> dtos = garmentService.getGarmentsByParam(searchDto);
-//
-//        return ResponseEntity.ok().body(dtos);
-//    }
 
     // CRUD Requests -- POST Requests
     @PostMapping("/producten/kleding")
