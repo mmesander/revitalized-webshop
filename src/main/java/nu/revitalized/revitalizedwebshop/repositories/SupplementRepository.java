58950f8de51ac44df9cc18aabc461eab8ec8f8d9
Mark Mesander <mark_mesander@hotmail.com>
2024-01-25 19:29:11 +0100

    remove unused method & imports

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
@@ -4,8 +4,6 @@ package nu.revitalized.revitalizedwebshop.repositories;
 import nu.revitalized.revitalizedwebshop.models.Supplement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import java.util.List;
 
 public interface SupplementRepository extends JpaRepository<Supplement, Long>, JpaSpecificationExecutor<Supplement> {
-    List<Supplement> findSupplementsByPriceLessThanEqual(Double price);
 }
