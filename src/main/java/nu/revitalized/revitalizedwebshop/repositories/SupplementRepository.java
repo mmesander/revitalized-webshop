a797c2c756d007e5eb81d177a925b81c46efbe75
Mark Mesander <mark_mesander@hotmail.com>
2024-01-25 18:24:01 +0100

    remove unused method from repo

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/SupplementRepository.java
@@ -10,18 +10,4 @@ import java.util.List;
 
 public interface SupplementRepository extends JpaRepository<Supplement, Long>, JpaSpecificationExecutor<Supplement> {
     List<Supplement> findSupplementsByPriceLessThanEqual(Double price);
-
-    @Query("SELECT s FROM Supplement s WHERE " +
-            "(:name is null or lower(s.name) like %:name%) and " +
-            "(:brand is null or lower(s.brand) like %:brand%) and " +
-            "(:price is null or s.price <= :price) and " +
-            "(:averageRating is null or s.averageRating >= :averageRating) and " +
-            "(:contains is null or s.contains like %:contains%)")
-    List<Supplement> findSupplementsByCriteria(
-            @Param("name") String name,
-            @Param("brand") String brand,
-            @Param("price") Double price,
-            @Param("averageRating") Double averageRating,
-            @Param("contains") String contains
-    );
 }
