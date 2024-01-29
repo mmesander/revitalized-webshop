48e8a0e3e0589ec5e9051f1f089bf4f371f523b3
Mark Mesander <mark_mesander@hotmail.com>
2024-01-29 11:53:03 +0100

    change name of helper method

--- src/main/java/nu/revitalized/revitalizedwebshop/helpers/CopyProperties.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/helpers/CopyProperties.java
@@ -3,7 +3,7 @@ package nu.revitalized.revitalizedwebshop.helpers;
 // Imports
 import org.springframework.beans.BeanUtils;
 
-public class PropertiesCopier {
+public class CopyProperties {
     public static <Input, Output> void copyProperties(Input source, Output destination) {
         BeanUtils.copyProperties(source, destination);
     }
