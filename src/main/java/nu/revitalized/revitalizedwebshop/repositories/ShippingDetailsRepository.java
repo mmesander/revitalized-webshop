d0f836cf0f8d62c063da5800f6e285d611045f30
Mark Mesander <mark_mesander@hotmail.com>
2024-01-29 10:51:30 +0100

    import JpaRepository and JpaSPecification in repo

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/ShippingDetailsRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/ShippingDetailsRepository.java
@@ -1,4 +1,9 @@
 package nu.revitalized.revitalizedwebshop.repositories;
 
-public interface ShippingDetailsRepository {
+// Imports
+import nu.revitalized.revitalizedwebshop.models.ShippingDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ShippingDetailsRepository extends JpaRepository<ShippingDetails, Long>, JpaSpecificationExecutor<ShippingDetails> {
 }
