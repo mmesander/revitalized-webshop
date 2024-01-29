7bdb099723fa903ba68a3d7e8060710ca880db71
Mark Mesander <mark_mesander@hotmail.com>
2024-01-29 12:03:50 +0100

    add shippingDetailsSpecification

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/specifications/ShippingDetailsSpecification.java
@@ -0,0 +1,26 @@
+package nu.revitalized.revitalizedwebshop.specifications;
+
+import nu.revitalized.revitalizedwebshop.models.ShippingDetails;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ShippingDetailsSpecification {
+    private ShippingDetailsSpecification() {}
+
+    // Request filter: Shipping Details shippingDetailsName
+    public static Specification<ShippingDetails> getShippingDetailsShippingDetailsNameLikeFilter(String shi)
+
+    // Request filter: Shipping Details name
+
+    // Request filter: Shipping Details country
+
+    // Request filter: Shipping Details city
+
+    // Request filter: Shipping Details zipCode
+
+    // Request filter: Shipping Details street
+
+    // Request filter: Shipping Details houseNumber
+
+    // Request filter: Shipping Details email
+
+}
