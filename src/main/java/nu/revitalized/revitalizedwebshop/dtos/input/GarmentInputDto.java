7bf0b4c747d0f17c425ff4741c5a0d3b88b694e7
Mark Mesander <mark_mesander@hotmail.com>
2024-02-06 17:52:10 +0100

    add messages to notNull annotations

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/GarmentInputDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/GarmentInputDto.java
@@ -18,9 +18,12 @@ public class GarmentInputDto {
     @NotNull
     @Size(min = 10, max = 200, message = "Description must be between 2 and 200 characters")
     private String description;
-    @NotNull
+    @NotNull(message = "Price is required")
     @Positive(message = "Price must be higher than zero")
     private Double price;
+    @NotNull(message = "Stock is required")
+    @Positive(message = "Stock can't be negative")
+    private Integer stock;
     @NotNull(message = "Size is required")
     private String size;
     @NotNull(message = "Color is required")
