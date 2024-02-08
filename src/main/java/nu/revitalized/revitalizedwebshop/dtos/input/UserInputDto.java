4560d9fb74f09046d6802db10dc5beb835a081ba
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 13:35:52 +0100

    implement ValidPassword in UserInputDto

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/UserInputDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/UserInputDto.java
@@ -5,13 +5,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import nu.revitalized.revitalizedwebshop.interfaces.ValidPassword;
 
 @Getter
 @Setter
 public class UserInputDto {
     @NotNull(message = "Username is required")
     private String username;
-
+    @ValidPassword
     private String password;
     @NotNull(message = "Email is required")
     @Email(message = "Enter a valid email")
