91850c9ccd2f450e383f8a8201fc71d1a45e1bab
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 17:30:32 +0100

    add AuthorityKey

--- src/main/java/nu/revitalized/revitalizedwebshop/security/GlobalCorsConfiguration.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/security/GlobalCorsConfiguration.java
@@ -1,5 +1,6 @@
 package nu.revitalized.revitalizedwebshop.security;
 
+// Imports
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
