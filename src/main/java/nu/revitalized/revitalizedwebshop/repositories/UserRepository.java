7ee5199ed491ce752522cc6999b74991cea62f48
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 14:21:03 +0100

    add method to repository

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/UserRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package nu.revitalized.revitalizedwebshop.repositories;
 
-public interface UserRepository {
+// Imports
+import nu.revitalized.revitalizedwebshop.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findByUsernameContainingIgnoreCase(String username);
 }
