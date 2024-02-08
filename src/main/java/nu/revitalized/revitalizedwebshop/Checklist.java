b148619e996361035b2184d520b2fab8261c5b8c
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 14:09:39 +0100

    add UserRepository and add checklist for personal use

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -0,0 +1,21 @@
+package nu.revitalized.revitalizedwebshop;
+
+public class Checklist {
+    //TODO: GlobalCorsConfiguration
+    //TODO: SpringSecurityConfig
+    //TODO: AuthenticationController
+    //TODO: UserController
+    //TODO: User Input & Output Dto's relations
+    //TODO: JwtRequestFilter
+    //TODO: Authority
+    //TODO: AuthorityKey
+    //TODO: AuthenticationRequest
+    //TODO: AuthenticationResponse
+    //TODO: AuthorityRespository
+    //TODO: CustomUserDetailService
+    //TODO: USerService
+    //TODO: JwtUtil
+    //TODO: RandomStringGenerator??
+    //TODO: return string in shipping details naar uppercase als hij al bestaat
+    //TODO: input dto's upgraden met de nieuwe interfaces
+}
