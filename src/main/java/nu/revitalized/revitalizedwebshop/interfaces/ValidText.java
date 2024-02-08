1bab54fe72e481a3616b335d812f91a92070a6bc
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 13:56:30 +0100

    add interface ValidText

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/interfaces/ValidText.java
@@ -0,0 +1,21 @@
+package nu.revitalized.revitalizedwebshop.interfaces;
+
+// Imports
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.ReportAsSingleViolation;
+import jakarta.validation.constraints.Pattern;
+import java.lang.annotation.*;
+
+@Pattern(regexp = "[a-zA-Z]+", message = "{fieldName} should contain only letters")
+@Constraint(validatedBy = {})
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ReportAsSingleViolation
+public @interface ValidText {
+    String message() default "{fieldName} should contain only letters";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String fieldName() default "Field";
+}
