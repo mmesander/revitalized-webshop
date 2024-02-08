b3de8de620fa442844d46d9e742f9c1382f51b36
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 13:59:04 +0100

    add interface ValidNumber

--- src/main/java/nu/revitalized/revitalizedwebshop/interfaces/ValidPassword.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/interfaces/ValidPassword.java
@@ -12,7 +12,7 @@ import java.lang.annotation.*;
 @NotNull
 @Size(min = 8, max = 20, message = "Password length must be between 8 and 20 characters")
 @Pattern.List({
-        @Pattern(regexp = ".*//d.*", message = "Password must contain a digit"),
+        @Pattern(regexp = ".*\\d.*", message = "Password must contain a digit"),
         @Pattern(regexp = ".*[a-z].*", message = "Password must contain a lowercase letter"),
         @Pattern(regexp = ".*[A-Z].*", message = "Password must contain an uppercase letter"),
         @Pattern(regexp = ".*[@#$%^&+=].*", message = "Password must contain a special character")
