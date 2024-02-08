c7568ea095c91acfd6fe62c7618bcadc1a9de27b
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 14:04:29 +0100

    add UserDto

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/UserDto.java
@@ -0,0 +1,16 @@
+package nu.revitalized.revitalizedwebshop.dtos.output;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    // Variables
+    private String username;
+    private String password;
+    private String email;
+
+    // Relations
+}
