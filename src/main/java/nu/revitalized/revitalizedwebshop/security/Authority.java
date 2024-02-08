904d6cce4021e617b8824fc46a5966bd96fc4c56
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 17:33:22 +0100

    add Authority class

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/security/Authority.java
@@ -0,0 +1,29 @@
+package nu.revitalized.revitalizedwebshop.security;
+
+// Imports
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@IdClass(AuthorityKey.class)
+@Table(name = "authorities")
+public class Authority implements Serializable {
+    @Id
+    @Column(nullable = false)
+    private String username;
+
+    @Id
+    @Column
+    private String authority;
+
+    public Authority() {}
+
+    public Authority(String username, String authority) {
+        this.username = username;
+        this.authority = authority;
+    }
+}
