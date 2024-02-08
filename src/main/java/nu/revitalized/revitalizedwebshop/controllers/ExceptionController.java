9c0cbf6d5fe092292ba4128f0e97593401ac9133
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 15:05:01 +0100

    add exceptions in controller

--- src/main/java/nu/revitalized/revitalizedwebshop/controllers/ExceptionController.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/controllers/ExceptionController.java
@@ -1,8 +1,10 @@
 package nu.revitalized.revitalizedwebshop.controllers;
 
 // Imports
+import nu.revitalized.revitalizedwebshop.exceptions.BadRequestException;
 import nu.revitalized.revitalizedwebshop.exceptions.InvalidInputException;
 import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
+import nu.revitalized.revitalizedwebshop.exceptions.UsernameNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +20,15 @@ public class ExceptionController {
     @ExceptionHandler(value = InvalidInputException.class)
     public ResponseEntity<Object> handleInvalidInputException(InvalidInputException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
