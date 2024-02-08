567f5a69e20b3e57845f2801e9de7c5151fc5f77
Mark Mesander <mark_mesander@hotmail.com>
2024-02-08 15:18:26 +0100

    add booleans to repository

--- src/main/java/nu/revitalized/revitalizedwebshop/repositories/UserRepository.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
+    boolean existsByUsernameIgnoreCaseAndEmailIgnoreCase(String username, String email);
+    boolean existsByUsernameIgnoreCase(String username);
+    boolean existsByEmailIgnoreCase(String email);
 }
