b109c5fa1999a9ee02a0cc2de807b36ce140fd92
Mark Mesander <mark_mesander@hotmail.com>
2024-02-09 17:28:50 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -10,6 +10,6 @@ public class Checklist {
     // Overig
     //TODO: return string in shipping details naar uppercase als hij al bestaat
     //TODO: alle contains veranderen naar containing in repo's en services of omzetten naar specifications
-    //TODO: in alle delete requests een string teruggeven dat iets verwijderd is
+    //TODO: in alle delete requests een string teruggeven dat iets verwijderd is (voorbeeld userService wederom)
     //TODO: Namen in methods veranderen in controller (voorbeeld bij usercontroller)
 }
