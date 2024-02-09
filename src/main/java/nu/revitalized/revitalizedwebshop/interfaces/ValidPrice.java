15150376c233569736c3f57b57791c6837c1ac70
Mark Mesander <mark_mesander@hotmail.com>
2024-02-09 13:02:08 +0100

    add interface validPrice

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/interfaces/ValidPrice.java
@@ -0,0 +1,19 @@
+package nu.revitalized.revitalizedwebshop.interfaces;
+
+// Imports
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.Digits;
+import java.lang.annotation.*;
+
+@Digits(integer = 10, fraction = 2, message = "{fieldName} should contain only digits")
+@Constraint(validatedBy = {})
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ValidPrice {
+    String message() default "{fieldName} should contain only digits";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String fieldName() default "Field";
+}
