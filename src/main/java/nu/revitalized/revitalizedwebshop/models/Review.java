f23f69ceae9e24e176dc922450d01d2eea3422ea
Mark Mesander <mark_mesander@hotmail.com>
2024-02-15 16:49:32 +0100

    add model

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/models/Review.java
@@ -0,0 +1,21 @@
+package nu.revitalized.revitalizedwebshop.models;
+
+// Imports
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews")
+public class Review {
+    // Variables
+    @SequenceGenerator(name = "reviews_seq", allocationSize = 1, initialValue = 10001)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reviews_seq")
+    private Long id;
+
+    private String review;
+    private Integer rating;
+}
