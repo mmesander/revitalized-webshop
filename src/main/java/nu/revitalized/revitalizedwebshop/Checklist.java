5fe1ac5343a40fd1e26d3880c15554bb61dab878
Mark Mesander <mark_mesander@hotmail.com>
2024-02-15 16:59:52 +0100

    add repository

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -2,9 +2,6 @@ package nu.revitalized.revitalizedwebshop;
 
 public class Checklist {
     // TODO: REVIEW
-    // TODO: Relations
-    // TODO: Model
-    // TODO: Input & Output DTO's
     // TODO: Repository
     // TODO: Specification
     // TODO: Service
