8ca5fd8baf04e708294460a68a6c94a1324611e1
Mark Mesander <mark_mesander@hotmail.com>
2024-02-15 16:44:06 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -2,7 +2,13 @@ package nu.revitalized.revitalizedwebshop;
 
 public class Checklist {
     // Relations
-    //
+    // Model
+    // Input & Output DTO's
+    // Repository
+    // Specification
+    // Service
+    // Controller
+    // Relations
 
     // Overig
     //
