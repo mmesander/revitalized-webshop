29e47fc0c3ce6746d56e283143c5bdb0f6931fa2
Mark Mesander <mark_mesander@hotmail.com>
2024-02-15 17:26:32 +0100

    add getReviewByID and getReviewsByParam methods

--- src/main/java/nu/revitalized/revitalizedwebshop/services/ReviewService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/ReviewService.java
@@ -2,16 +2,19 @@ package nu.revitalized.revitalizedwebshop.services;
 
 // Imports
 import static nu.revitalized.revitalizedwebshop.helpers.CopyProperties.copyProperties;
+import static nu.revitalized.revitalizedwebshop.specifications.ReviewSpecification.*;
 import nu.revitalized.revitalizedwebshop.dtos.input.ReviewInputDto;
 import nu.revitalized.revitalizedwebshop.dtos.output.ReviewDto;
 import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
 import nu.revitalized.revitalizedwebshop.models.Review;
 import nu.revitalized.revitalizedwebshop.repositories.ReviewRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService {
@@ -58,5 +61,40 @@ public class ReviewService {
         }
     }
 
+    public ReviewDto getReview(Long id) {
+        Optional<Review> review = reviewRepository.findById(id);
 
+        if (review.isPresent()) {
+            return reviewToDto(review.get());
+        } else {
+            throw new RecordNotFoundException("Review with id: " + id + " not found");
+        }
+    }
+
+    public List<ReviewDto> getReviewsByParam(
+            Integer rating,
+            Integer minRating,
+            Integer maxRating
+    ) {
+        Specification<Review> params = Specification.where
+                (rating == null ? null : getReviewRatingLikeFilter(rating))
+                .and(minRating == null ? null : getReviewMoreThanFilter(minRating))
+                .and(maxRating == null ? null : getReviewLessThanFilter(maxRating));
+
+        List<Review> filteredReviews = reviewRepository.findAll(params);
+        List<ReviewDto> reviewDtos = new ArrayList<>();
+
+        for (Review review : filteredReviews) {
+            ReviewDto reviewDto = reviewToDto(review);
+            reviewDtos.add(reviewDto);
+        }
+
+        if (reviewDtos.isEmpty()) {
+            throw new RecordNotFoundException("No reviews found with the specified filters");
+        } else {
+            reviewDtos.sort(Comparator.comparing(ReviewDto::getId).reversed());
+
+            return reviewDtos;
+        }
+    }
 }
