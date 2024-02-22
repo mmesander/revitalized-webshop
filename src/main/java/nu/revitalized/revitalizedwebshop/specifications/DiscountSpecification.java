7bda91fad4194ee1a1b3ffb1e20d26f994c6051e
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 16:35:14 +0100

    fix typo's in shippingDetails Specification and add empty DiscountSpecification

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/specifications/DiscountSpecification.java
@@ -0,0 +1,8 @@
+package nu.revitalized.revitalizedwebshop.specifications;
+
+public class DiscountSpecification {
+    private DiscountSpecification() {}
+
+    // Request filter: Discount name
+    // Request fi
+}
