122d2a89aac577fd55ca069b184e6a74d63af137
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 16:41:42 +0100

    add empty methods

--- src/main/java/nu/revitalized/revitalizedwebshop/specifications/DiscountSpecification.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/specifications/DiscountSpecification.java
@@ -1,8 +1,20 @@
 package nu.revitalized.revitalizedwebshop.specifications;
 
+import nu.revitalized.revitalizedwebshop.models.Discount;
+import org.springframework.data.jpa.domain.Specification;
+
 public class DiscountSpecification {
     private DiscountSpecification() {}
 
-    // Request filter: Discount name
-    // Request fi
+    // Request Filter: Discount name
+    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
+
+    // Request Filter: Discount value
+    public static Specification<Discount> getDiscountValueLikeFilter(Double valueLike) {}
+
+    // Request Filter: Discount minValue
+    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
+
+    // Request Filter: Discount maxValue
+    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
 }
