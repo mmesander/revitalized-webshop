3b6b214b1e32cf6d305a4d194a345a73e2d902cc
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 16:46:48 +0100

    add discountSpecifications

--- src/main/java/nu/revitalized/revitalizedwebshop/specifications/DiscountSpecification.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/specifications/DiscountSpecification.java
@@ -4,17 +4,31 @@ import nu.revitalized.revitalizedwebshop.models.Discount;
 import org.springframework.data.jpa.domain.Specification;
 
 public class DiscountSpecification {
-    private DiscountSpecification() {}
+    private DiscountSpecification() {
+    }
 
     // Request Filter: Discount name
-    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
+    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {
+        String formattedNameLike = "%" + nameLike.toLowerCase() + "%";
+        return ((root, query, criteriaBuilder) -> criteriaBuilder
+                .like(criteriaBuilder.lower(root.get("name")), formattedNameLike));
+    }
 
     // Request Filter: Discount value
-    public static Specification<Discount> getDiscountValueLikeFilter(Double valueLike) {}
+    public static Specification<Discount> getDiscountValueLikeFilter(Double valueLike) {
+        return (((root, query, criteriaBuilder) -> criteriaBuilder
+                .equal(root.get("value"), valueLike)));
+    }
 
     // Request Filter: Discount minValue
-    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
+    public static Specification<Discount> getDiscountValueMoreThanFilter(String minValueLike) {
+        return (((root, query, criteriaBuilder) -> criteriaBuilder
+                .greaterThanOrEqualTo(root.get("price"), minValueLike)));
+    }
 
     // Request Filter: Discount maxValue
-    public static Specification<Discount> getDiscountNameLikeFilter(String nameLike) {}
+    public static Specification<Discount> getDiscountValueLessThanFilter(String maxValueLike) {
+        return (((root, query, criteriaBuilder) -> criteriaBuilder
+                .lessThanOrEqualTo(root.get("price"), maxValueLike)));
+    }
 }
