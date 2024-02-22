591018f5dfb2d8416ac51955ecf1913916d36469
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 14:30:22 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -17,6 +17,13 @@ public class Checklist {
     // Overig
     // TODO: User (Athenticated) Get ShippingDetails BY ID
     // TODO: De url van authenticate aanpassen op users/authenticate
+    // TODO: Zorgen dat de reviews ook worden toegevoegd als je de super ophaalt
+    // TODO: -- Door in de productDto de reviews toe te voegen en ze in de transfer methods in product service erin knallen
     // TODO: Foto's toevoegen
     // TODO: Alles nalopen op imports etc.
+
+
+    // Rowan
+    // TODO: Vragen hoe het zit met dat UML Diagram en authorityKey en of de attributes in Authority kloppen
+    // TODO: Vragen hoe de foto's geimporteerd worden
 }
