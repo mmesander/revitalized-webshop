42cfbcb74eff5e5ac054541f99d7f36e53717e04
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 14:50:11 +0100

    add model

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/models/Order.java
@@ -0,0 +1,28 @@
+package nu.revitalized.revitalizedwebshop.models;
+
+// Imports
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    // Variables
+    @SequenceGenerator(name = "orders_seq", allocationSize = 1, initialValue = 101)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orders_seq")
+    private Long orderNumber;
+    private Date orderDate;
+    private String discountCode;
+    private Double totalAmount;
+    private String status;
+    private boolean isPayed;
+
+    // Relations
+
+}
