4336c56a652a1892674a9ee5553fc4f54a9b3e11
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 16:13:49 +0100

    add outputDto

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/DiscountDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/DiscountDto.java
@@ -1,4 +1,13 @@
 package nu.revitalized.revitalizedwebshop.dtos.output;
 
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DiscountDto {
+    private Long id;
+    private String name;
+    private Double value;
 }
