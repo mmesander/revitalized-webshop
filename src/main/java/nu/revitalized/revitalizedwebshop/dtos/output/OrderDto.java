199c936c8bb7764ecb9565a5f4429c2529c36db3
Mark Mesander <mark_mesander@hotmail.com>
2024-02-22 14:52:25 +0100

    add outputDto

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/output/OrderDto.java
@@ -0,0 +1,18 @@
+package nu.revitalized.revitalizedwebshop.dtos.output;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+public class OrderDto {
+    // Variables
+    private Long orderNumber;
+    private Date orderDate;
+    private String discountCode;
+    private Double totalAmount;
+    private String status;
+    private boolean isPayed;
+}
