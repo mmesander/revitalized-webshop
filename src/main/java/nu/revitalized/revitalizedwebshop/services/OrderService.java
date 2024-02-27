63ac91a6ed8ea2cd7a7ffb6e9eaad3c981feecf9
Mark Mesander <mark_mesander@hotmail.com>
2024-02-27 13:27:37 +0100

    add getAllOrders and transfer method

--- src/main/java/nu/revitalized/revitalizedwebshop/services/OrderService.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/services/OrderService.java
@@ -2,11 +2,17 @@ package nu.revitalized.revitalizedwebshop.services;
 
 // Imports
 import static nu.revitalized.revitalizedwebshop.helpers.CopyProperties.copyProperties;
+
+import nu.revitalized.revitalizedwebshop.dtos.input.OrderInputDto;
 import nu.revitalized.revitalizedwebshop.dtos.output.OrderDto;
+import nu.revitalized.revitalizedwebshop.exceptions.RecordNotFoundException;
 import nu.revitalized.revitalizedwebshop.models.Order;
 import nu.revitalized.revitalizedwebshop.repositories.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -18,6 +24,14 @@ public class OrderService {
     }
 
     // Transfer Methods
+    public static Order dtoToOrder(OrderInputDto inputDto) {
+        Order order = new Order();
+
+        copyProperties(inputDto, order);
+
+        return order;
+    }
+
     public static OrderDto orderToDto(Order order) {
         OrderDto orderDto = new OrderDto();
 
@@ -27,7 +41,24 @@ public class OrderService {
     }
 
     // CRUD Methods
-    public List<Order> getAllOrders() {}
+    public List<OrderDto> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        List<OrderDto> orderDtos = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderDto orderDto = orderToDto(order);
+            orderDtos.add(orderDto);
+        }
+
+        if (orderDtos.isEmpty()) {
+            throw new RecordNotFoundException("No orders found");
+        } else {
+            orderDtos.sort(Comparator.comparing(OrderDto::getOrderNumber));
+
+            return orderDtos;
+        }
+    }
+
     public OrderDto getOrderById() {}
     public List<Order> getALlOrdersByParam() {}
     public List<Order> getAllPayedOrders() {}
