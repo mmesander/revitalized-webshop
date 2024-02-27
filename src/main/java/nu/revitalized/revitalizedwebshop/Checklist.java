6c268c0d18ad288a9566a3839ec526339919e768
Mark Mesander <mark_mesander@hotmail.com>
2024-02-27 13:43:31 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -17,7 +17,6 @@ public class Checklist {
     // TODO: -- Door in de productDto de reviews toe te voegen en ze in de transfer methods in product service erin knallen
     // TODO: Foto's toevoegen
     // TODO: Alles nalopen op imports etc.
-    // TODO: een BuildIdNotFound helper maken die een string geeft
     // TODO: checken in de services of de methods bij de owner of target kant staan (discount voorbeeld)
 
 
