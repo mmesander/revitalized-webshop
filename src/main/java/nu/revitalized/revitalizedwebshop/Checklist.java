a69c26463206c659efec33f53a799e7304ba3c95
Mark Mesander <mark_mesander@hotmail.com>
2024-02-28 01:01:01 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -3,52 +3,31 @@ package nu.revitalized.revitalizedwebshop;
 public class Checklist {
     // ORDER
     // TODO: Relations:
-    // TODO: Relation - Products
-    // TODO: Relation - ShippingDetails
-    // TODO: Relation - User
-
-
-    // Overig
-    // TODO: User (Athenticated) Get ShippingDetails BY ID
-    // TODO: Zorgen dat de reviews ook worden toegevoegd als je de super ophaalt
-    // TODO: -- Door in de productDto de reviews toe te voegen en ze in de transfer methods in product service erin knallen
-    // TODO: Foto's toevoegen
-    // TODO: Alles nalopen op imports etc.
-    // TODO: checken in de services of de methods bij de owner of target kant staan (discount voorbeeld)
-    // TODO: de shipping details op een andere URL zetten /users/shipping-details
-
-
-    // Rowan
-    // TODO: Vragen hoe de foto's geimporteerd worden
-
-
-    // assign products to order
-    // remove products from order
-
-    // assign order to user
-    // remove order from user
-
-    // assign shipping details to order
-    // update shipping details from order
-    // patch shipping details from order
-    // remove shipping details from order
-
-
-
-    // USER (auth)
-    // Get all personal orders (wellicht met een shortdto -> id, datum, prijs)
-    // get user specific order (met alle data)
-
-    // Create Order
-    // Add products to order
-    // Remove products from order
-
-    // assign shippingDetails to order
-    // post new shipping details for order
-    // update shipping details for order?
+    // TODO: AssignShippingDetailsToOrder
+    // TODO: RemoveShippingDetailsFromOrder
+    // TODO: User (Auth) Requests in userService:
+    // TODO: getAllAuthUserOrders()
+    // TODO: getAuthUserOrderById()
+    // TODO: createAuthUserOrder()
+    // TODO: addProductsToAuthUserOrder()
+    // TODO: removeProductsFromAuthUserOrder()
+    // TODO: assignShippingDetailsToAuthUserOrder()
+    // TODO: removeShippingDetailsFromAuthUserOrder()
+    // TODO: addDiscountToAuthUserOrder()
+    // TODO: finishAuthUserOrder()
 
     // Finish order Send/Pay order (hier de discount bij)
     // Deze kan niet verzonden worden indien er geen shippingdetails bij zit!
     // En ook niet als er geen producten in zitten
     // En ook niet als de discount niet geldig is
+
+    // Overig
+    // TODO: Order Relation - ShippingDetails
+    // TODO: Foto's toevoegen
+    // TODO: Alles nalopen op fouten
+    // TODO: Alles nalopen op imports etc.
+
+    // Rowan
+    // TODO: De fout met rowan in postman checken (gericht op shippingdetails)
+    // TODO: Vragen hoe de foto's geimporteerd worden
 }
