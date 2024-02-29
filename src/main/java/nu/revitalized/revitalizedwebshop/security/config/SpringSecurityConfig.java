523147fab918f823963b94932535f3eecb3e5a2c
Mark Mesander <mark_mesander@hotmail.com>
2024-02-29 17:59:32 +0100

    update security config

--- src/main/java/nu/revitalized/revitalizedwebshop/security/config/SpringSecurityConfig.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/security/config/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+    public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
         var auth = new DaoAuthenticationProvider();
         auth.setPasswordEncoder(passwordEncoder);
         auth.setUserDetailsService(customUserDetailService);
@@ -104,12 +104,12 @@ public class SpringSecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
 
-                        .requestMatchers(HttpMethod.GET, "products/reviews").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "products/reviews/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "products/reviews").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "products/reviews/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "products/reviews/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "products/reviews/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/products/reviews").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/products/reviews/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/products/reviews").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/products/reviews/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/products/reviews/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/products/reviews/**").hasRole("ADMIN")
 
                         .requestMatchers(HttpMethod.POST, "/products/garments").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/products/garments/**").hasRole("ADMIN")
