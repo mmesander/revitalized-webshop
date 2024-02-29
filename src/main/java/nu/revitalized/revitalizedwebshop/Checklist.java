947fa03da1124d3c420e1189490c227547cdab22
Mark Mesander <mark_mesander@hotmail.com>
2024-02-29 16:32:40 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -22,6 +22,7 @@ public class Checklist {
     // En ook niet als de discount niet geldig is
 
     // Overig
+    // TODO: checken of ik op de juiste plekken post/put heb gebruikt en de securityconfig updaten
     // TODO: Order Relation - ShippingDetails
     // TODO: Foto's toevoegen
     // TODO: Alles nalopen op fouten
