b2c3202f2f011cc442bd3c131abef6b0023f853b
Mark Mesander <mark_mesander@hotmail.com>
2024-03-05 21:31:39 +0100

    update calculateTotalAmount helper

--- src/main/java/nu/revitalized/revitalizedwebshop/helpers/CalculateTotalAmount.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/helpers/CalculateTotalAmount.java
@@ -7,14 +7,14 @@ import nu.revitalized.revitalizedwebshop.models.Supplement;
 
 public class CalculateTotalAmount {
     public static Double calculateTotalAmount(Order order) {
-        Double totalAmount = 0.00;
+        double totalAmount = 0.00;
 
         for (Supplement supplement : order.getSupplements()) {
-            totalAmount = totalAmount + supplement.getPrice();
+            totalAmount += supplement.getPrice();
         }
 
         for (Garment garment : order.getGarments()) {
-            totalAmount = totalAmount + garment.getPrice();
+            totalAmount += garment.getPrice();
         }
 
         return totalAmount;
