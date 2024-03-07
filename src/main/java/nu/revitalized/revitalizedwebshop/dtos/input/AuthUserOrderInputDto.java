b2d495966e7bab986a0b1722009b5fb221ae1c9d
Mark Mesander <mark_mesander@hotmail.com>
2024-03-07 16:08:25 +0100

    update inputdto

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/AuthUserOrderInputDto.java
@@ -0,0 +1,13 @@
+package nu.revitalized.revitalizedwebshop.dtos.input;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class AuthUserOrderInputDto {
+    private List<O>
+}
--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/MultipleIdInputDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/MultipleIdInputDto.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Getter
 @Setter
 public class MultipleIdInputDto {
-    @NotNull(message = "Id is required")
+    @NotNull(message = "ProductIds is required")
     private List<Long> productIds;
 }
