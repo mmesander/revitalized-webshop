6661dd6854a7550705234a1a67deff875bd766d3
Mark Mesander <mark_mesander@hotmail.com>
2024-03-07 16:10:22 +0100

    add AuthUserOrderInputDto

--- src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/AuthUserOrderInputDto.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/dtos/input/AuthUserOrderInputDto.java
@@ -3,11 +3,12 @@ package nu.revitalized.revitalizedwebshop.dtos.input;
 // Imports
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
 @Getter
 @Setter
 public class AuthUserOrderInputDto {
-    private List<O>
+    private List<Long> productIds;
+    private Long shippingDetailsId;
+    private String discountCode;
 }
