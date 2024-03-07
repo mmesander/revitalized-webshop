dc6c1c71dcff57a69ae34989ce7597c18c4b8334
Mark Mesander <mark_mesander@hotmail.com>
2024-03-07 17:40:39 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -2,20 +2,7 @@ package nu.revitalized.revitalizedwebshop;
 
 public class Checklist {
     // ORDER
-    // TODO: User (Auth) Requests in userService:
-    // TODO: getAllAuthUserOrders()
-    // TODO: getAuthUserOrderById()
-    // TODO: createAuthUserOrder()
-    // TODO: addProductsToAuthUserOrder()
-    // TODO: assignShippingDetailsToAuthUserOrder()
-    // TODO: removeShippingDetailsFromAuthUserOrder()
-    // TODO: addDiscountToAuthUserOrder()
-    // TODO: finishAuthUserOrder()
-
-    // Finish order Send/Pay order (hier de discount bij)
-    // Deze kan niet verzonden worden indien er geen shippingdetails bij zit!
-    // En ook niet als er geen producten in zitten
-    // En ook niet als de discount niet geldig is
+    // TODO: User (Auth) Requests in userController:
 
     // Overig
     // TODO: Foto's toevoegen
