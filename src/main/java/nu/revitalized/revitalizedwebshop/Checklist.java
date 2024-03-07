1a8866aea892c83e02639b3a5bb73db64fa44c64
Mark Mesander <mark_mesander@hotmail.com>
2024-03-08 00:13:47 +0100

    remove checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++
@@ -1,9 +0,0 @@
-package nu.revitalized.revitalizedwebshop;
-
-public class Checklist {
-    // Overig
-    // TODO: Foto's toevoegen
-
-    // Rowan
-    // TODO: Vragen hoe de foto's geimporteerd worden
-}
