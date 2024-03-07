b7e1faf4e9898a69bb2996e19e931266260dc372
Mark Mesander <mark_mesander@hotmail.com>
2024-03-07 20:53:50 +0100

    add repository

---
+++ src/main/java/nu/revitalized/revitalizedwebshop/repositories/ImageDataRepository.java
@@ -0,0 +1,8 @@
+package nu.revitalized.revitalizedwebshop.repositories;
+
+// Imports
+import nu.revitalized.revitalizedwebshop.models.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageDataRepository extends JpaRepository<ImageData, Long> {
+}
