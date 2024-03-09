0414564db4503ca9738a81cfcefb89cfedc9b25a
Mark Mesander <mark_mesander@hotmail.com>
2024-03-09 16:36:04 +0100

    update checklist

--- src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/Checklist.java
@@ -1,7 +1,6 @@
 package nu.revitalized.revitalizedwebshop;
 
 public class Checklist {
-    //TODO: JSON postman checken en afronden
     //TODO: Wederom alle imports, formatting en code nalopen
     //TODO: Testen schrijven
     //TODO: Sequence DIagram maken
