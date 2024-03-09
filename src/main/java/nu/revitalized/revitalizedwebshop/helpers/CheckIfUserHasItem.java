9579e0fea811b8db68e5cf71e9f1d7f50889bc1d
Mark Mesander <mark_mesander@hotmail.com>
2024-03-09 15:22:25 +0100

    add checkIfUserHasShippingDetails to helper checkIfUserHasItem class

--- src/main/java/nu/revitalized/revitalizedwebshop/helpers/CheckIfUserHasItem.java
+++ src/main/java/nu/revitalized/revitalizedwebshop/helpers/CheckIfUserHasItem.java
@@ -3,6 +3,7 @@ package nu.revitalized.revitalizedwebshop.helpers;
 // Imports
 import nu.revitalized.revitalizedwebshop.exceptions.BadRequestException;
 import nu.revitalized.revitalizedwebshop.models.Review;
+import nu.revitalized.revitalizedwebshop.models.ShippingDetails;
 import nu.revitalized.revitalizedwebshop.models.User;
 import java.util.List;
 
@@ -21,6 +22,23 @@ public class CheckIfUserHasItem {
         if (!hasReview) {
             throw new BadRequestException("User: " + user.getUsername()
                     + " does not have review with id: " + review.getId());
+        }
+    }
+
+    public static void checkIfUserHasShippingDetails(User user, ShippingDetails shippingDetails) {
+        List<ShippingDetails> shippingDetailsList = user.getShippingDetails();
+        boolean hasShippingDetails = false;
+
+        for (ShippingDetails foundShippingDetails : shippingDetailsList) {
+            if (foundShippingDetails.equals(shippingDetails)) {
+                hasShippingDetails = true;
+                break;
+            }
+        }
+
+        if (!hasShippingDetails) {
+            throw new BadRequestException("User: " + user.getUsername()
+            + " does not have shipping details with id: " + shippingDetails.getId());
         }
     }
 }
